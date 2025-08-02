6d58c433dec9f975392ee88e07ccc887e4756655
LeandroDevLab <prof.oliota@gmail.com>
2025-08-01 22:24:47 -0300

    express get e post http web

---
+++ secao10-backend/Node/a6/server.js
@@ -0,0 +1,35 @@
+//express é um micro framework que ajuda a trabalhar com a web
+const express = require('express');
+
+// o nome app para executar o express é uma convenção!
+const app = express();
+
+//  CRUD -> Create, Read, Update, Delete
+//operações  post    get   put    delete
+
+// http://meusite.com/
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recebi o formulário!');
+});
+
+//criando outra rota (uma página para cada rota)
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato com a gente!');
+});
+
+//a função é opcional, mas cria o link clicável e ainda uma mensagem
+// que o servidor está rodando
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
