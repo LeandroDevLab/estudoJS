3b1922764774e12a1ffbc84e1d818041480ded1d
LeandroDevLab <prof.oliota@gmail.com>
2025-08-06 21:46:11 -0300

    Projeto agenda: rota de login, loginController, página de login

--- secao11/projeto_agenda/src/middlewares/middleware.js
+++ secao11/projeto_agenda/src/middlewares/middleware.js
@@ -10,9 +10,10 @@ exports.outroMiddlewareGlobal = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if (err && err.code === 'EBADCSRFTOKEN') {
+  if (err) {
     return res.render('404');
   }
+  next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
