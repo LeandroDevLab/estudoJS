4a3abbd1acf17a3916a82b5c60e473483728a1d8
LeandroDevLab <prof.oliota@gmail.com>
2025-08-08 15:51:19 -0300

    Projeto agenda: desafio 1 - validação Frontend (email, senha)

--- secao11/projeto_agenda/frontend/main.js
+++ secao11/projeto_agenda/frontend/main.js
@@ -1,7 +1,14 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './validators/Login';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+login.init();
+cadastro.init();
+//import './assets/css/style.css';
 
 //para atualizar aqui precisei rodar o npm run dev para atualizar as mudanças no bundle.js
 //alert(1);
