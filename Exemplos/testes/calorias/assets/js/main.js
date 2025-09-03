dbc82cb94cde1ed70c5c6be3f016ed32fe2565a2
LeandroDevLab <prof.oliota@gmail.com>
2025-09-03 13:37:47 -0300

    refactor: encapsulamento ContadorCalorias

--- Exemplos/testes/calorias/assets/js/main.js
+++ Exemplos/testes/calorias/assets/js/main.js
@@ -1,45 +1,49 @@
-const lista = [];
+class ContadorCalorias {
+  constructor() {
+    this.lista = [];
+    this.formulario = document.querySelector('.form');
+    this.divLista = document.querySelector('.lista');
+    this.init();
+  }
 
-const formulario = document.querySelector('.form');
-const inputKcal = document.getElementById('kcal');
-const inputCarbo = document.getElementById('carboidrato');
-const inputProtein = document.getElementById('proteina');
-const inputGordura = document.getElementById('gordura');
-const botao = document.querySelector('.botao');
-const divLista = document.querySelector('.lista');
+  init() {
+    this.formulario.addEventListener('submit', e => {
+      e.preventDefault();
+      this.addItem();
+      this.preencheDivLista();
+      this.formulario.reset();
+    });
+  }
 
-function addItem() {
-  let kcal = inputKcal.value;
-  let carbo = inputCarbo.value;
-  let proteina = inputProtein.value;
-  let gordura = inputGordura.value;
-  const item = {
-    kcal,
-    carbo,
-    proteina,
-    gordura,
-  };
-  lista.push(item);
-  console.log(item);
-}
+  addItem() {
+    let kcal = this.formulario.querySelector('#kcal').value;
+    let carbo = this.formulario.querySelector('#carboidrato').value;
+    let proteina = this.formulario.querySelector('#proteina').value;
+    let gordura = this.formulario.querySelector('#gordura').value;
 
-function preencheDivLista() {
-  divLista.innerHTML = '';
-  for (item of lista) {
-    let div = document.createElement('div');
-    div.innerHTML = `<p>
-      Kcal: ${item.kcal} 
-      Carbo: ${item.carbo} 
-      Proteina: ${item.proteina} 
-      Gordura: ${item.gordura} 
-      </p>`;
-    divLista.appendChild(div);
+    this.lista.push({
+      kcal,
+      carbo,
+      proteina,
+      gordura,
+    });
+    console.log(this.lista);
+  }
+
+  preencheDivLista() {
+    this.divLista.innerHTML = ``;
+    console.log('lista => ', this.lista.length);
+    for (let i = 0; i < this.lista.length; i++) {
+      let div = document.createElement('div');
+      div.innerHTML = `<p>
+        Kcal: ${this.lista[i].kcal} 
+        Carbo: ${this.lista[i].carbo} 
+        Proteina: ${this.lista[i].proteina} 
+        Gordura: ${this.lista[i].gordura} 
+        </p>`;
+      this.divLista.appendChild(div);
+    }
   }
 }
 
-botao.addEventListener('click', () => {
-  addItem();
-  preencheDivLista();
-
-  console.log(lista);
-});
+const contador = new ContadorCalorias();
